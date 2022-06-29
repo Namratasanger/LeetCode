5c60db2613027bf9cc46056e69fd3db9090f6a2c
Namrata Sanger <35221403+Namratasanger@users.noreply.github.com>
2022-06-29 18:33:46 -0400

    Optimized code with 71ms.

--- ProgrammingSkill/CountOddNumbers.js
+++ ProgrammingSkill/CountOddNumbers.js
@@ -1,13 +1,9 @@
-/**
- * @param {number} low
- * @param {number} high
- * @return {number}
- */
 var countOdds = function(low, high) {
-    let count = Math.floor((high - low)/2);
     // if low or high is odd
-    if (low % 2 != 0 || high % 2 != 0)
-        count += 1;
- 
-    return count;
+    if (low % 2 != 0 || high % 2 != 0){
+        return Math.floor((high - low)/2) + 1;        
+    }
+    else{
+        return Math.floor((high - low)/2);        
+    }
 };
