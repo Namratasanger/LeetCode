64ca4901aec4b1d310b60d090acb7fbba0b4265a
Namrata Sanger <35221403+Namratasanger@users.noreply.github.com>
2022-08-21 20:16:27 -0400

    Update Kth Missing Positive Integer.js

--- DailyCodingChallenge/Kth Missing Positive Integer.js
+++ DailyCodingChallenge/Kth Missing Positive Integer.js
@@ -1,25 +1,42 @@
 /*
-Day 4
+Day 5
 1539. Kth Missing Positive Number
-Link: https://leetcode.com/problems/kth-missing-positive-number/
+Leet code: https://leetcode.com/problems/kth-missing-positive-number/
 */
-
 
 /**
  * @param {number[]} arr
  * @param {number} k
  * @return {number}
  */
+// time complexity  - O(N)
 var findKthPositive = function(arr, k) {
-    let count = 0;
     let number = 1;
     let digit;
-    while(count != k){
+    while(k>0){
         if(!arr.includes(number)){
             digit = number;
-            count++;
+            k--;
         }
         number++;
     }
     return digit;
+};
+
+
+// time complexity  - O(logN)
+var findKthPositive = function(arr, k) {
+    let low = 0;
+    let high = arr.length - 1;
+    let closetMid = 0;
+    while(low <= high){
+        let mid = low + Math.floor((high - low)/2);
+        if(arr[mid] - (mid+1) < k){
+            closetMid = mid + 1;
+            low = mid + 1;
+        }else{
+            high = mid - 1;
+        }
+    }
+    return k + closetMid;
 };
